112c79b30110e1ee98a8ea9396e77a2e9563b066
Talon Hughes <talon.hughes@gmail.com>
2016-10-01 09:54:00 -0600

    finished app1

---
+++ app1-basics/controller.js
@@ -0,0 +1,3 @@
+angular.module('appOne').controller('mainControl', function($scope, mainService){
+
+});
--- app1-basics/service.js
+++ app1-basics/service.js
@@ -1,1 +1,6 @@
+angular.module('appOne').service('mainService', function(){
 
+  this.fullname = function(obj){
+    fullname = obj;
+  };
+});
